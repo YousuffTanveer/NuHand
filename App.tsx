c1ffaa6cf7132ac26112b94a8dbeae65b71bbb93
bobby.whaler@icloud.com <you@example.com>
2023-01-23 16:58:47 +0000

    .gitignore

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start!
+      </Text>
+      <Text>
+        hello
+      </Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
