260448d0f8ab1923f3ccfdc57b585c2ee5b7c17b
Yousuff <yousufftanveer77@gmail.com>
2023-01-30 09:53:17 +0000

    implements third party api to retrieve real time currency exchange values

--- App.tsx
+++ App.tsx
@@ -1,50 +1,64 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-import { NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LogInScreen from "./Screens/LogInScreen";
 import HomeScreen from "./Screens/HomeScreen";
 import SignUpScreen from "./Screens/SignUpScreen";
-import WelcomeScreen from "./Screens/WelcomeScreen"
+import WelcomeScreen from "./Screens/WelcomeScreen";
 import Listings from "./Screens/Listings";
 import Account from "./Screens/Account";
 
 type RootStackParamList = {
-  Login: undefined,
-  Home: undefined
+  Login: undefined;
+  Home: undefined;
 };
-
-
-
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
 
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   const [selectedCurrency, setSelectedCurrency] = useState("");
 
-const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
   return (
-   
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name='SignUp' options={{ title: 'SignUp' }}>
-  {(props) => <SignUpScreen {...props} firstName={firstName} setFirstName={setFirstName}/>}
-</Stack.Screen>
-        <Stack.Screen name='Welcome' options={{ title: 'Welcome' }}>
-  {(props) => <WelcomeScreen {...props}  firstName={firstName} />}
-</Stack.Screen>
-        <Stack.Screen name='Login' options={{ title: 'Login' }}>
-  {(props) => <LogInScreen {...props} user={user} setUser={setUser} />}
-  </Stack.Screen>
-        <Stack.Screen name='Home' options={{ title: 'Home' }}>
-  {(props) => <HomeScreen {...props} selectedCurrency={selectedCurrency} setSelectedCurrency={setSelectedCurrency} />}
-  </Stack.Screen>
-        <Stack.Screen name="Listings" options={{ title: 'Listings' }}>
-  {(props) => <Listings {...props} selectedCurrency={selectedCurrency} setSelectedCurrency={setSelectedCurrency} />}
-   </Stack.Screen>
+        <Stack.Screen name="SignUp" options={{ title: "SignUp" }}>
+          {(props) => (
+            <SignUpScreen
+              {...props}
+              firstName={firstName}
+              setFirstName={setFirstName}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Welcome" options={{ title: "Welcome" }}>
+          {(props) => <WelcomeScreen {...props} firstName={firstName} />}
+        </Stack.Screen>
+        <Stack.Screen name="Login" options={{ title: "Login" }}>
+          {(props) => <LogInScreen {...props} user={user} setUser={setUser} />}
+        </Stack.Screen>
+        <Stack.Screen name="Home" options={{ title: "Home" }}>
+          {(props) => (
+            <HomeScreen
+              {...props}
+              selectedCurrency={selectedCurrency}
+              setSelectedCurrency={setSelectedCurrency}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Listings" options={{ title: "Listings" }}>
+          {(props) => (
+            <Listings
+              {...props}
+              selectedCurrency={selectedCurrency}
+              setSelectedCurrency={setSelectedCurrency}
+            />
+          )}
+        </Stack.Screen>
         <Stack.Screen name="Account" component={Account} />
       </Stack.Navigator>
     </NavigationContainer>
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,52 +1,104 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
 
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list'
+const HomeScreen = ({ navigation, selectedCurrency, setSelectedCurrency }) => {
+  interface conversionProps {
+    amount: number;
+    base: string;
+    date: string;
+    rates: { [key: string]: number };
+  }
 
-const HomeScreen = ( { navigation, selectedCurrency, setSelectedCurrency } ) => {
+  const [conversion, setConversion] = useState<conversionProps | null>(null);
 
+  const [conversionValue, setConversionValue] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (conversion !== null) {
+      const value = conversion.rates[selectedCurrency];
+      setConversionValue(value);
+    }
+  }, [selectedCurrency]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.frankfurter.app/latest?from=GBP")
+      .then((res) => setConversion(res.data));
+  }, []);
 
   // api data:
   const data = [
-    {key:'1', value:'USD'},
-    {key:'2', value:'EUR'},
-]
+    { key: "1", value: "AUD" },
+    { key: "2", value: "BGN" },
+    { key: "3", value: "BRL" },
+    { key: "4", value: "CAD" },
+    { key: "5", value: "CHF" },
+    { key: "6", value: "CNY" },
+    { key: "7", value: "CZK" },
+    { key: "8", value: "DKK" },
+    { key: "9", value: "EUR" },
+    { key: "10", value: "HKD" },
+    { key: "11", value: "HUF" },
+    { key: "12", value: "IDR" },
+    { key: "13", value: "ILS" },
+    { key: "14", value: "INR" },
+    { key: "15", value: "ISK" },
+    { key: "16", value: "JPY" },
+    { key: "17", value: "KRW" },
+    { key: "18", value: "MXN" },
+    { key: "19", value: "MYR" },
+    { key: "20", value: "NOK" },
+    { key: "21", value: "NZD" },
+    { key: "22", value: "PHP" },
+    { key: "23", value: "PLN" },
+    { key: "24", value: "RON" },
+    { key: "25", value: "SEK" },
+    { key: "26", value: "SGD" },
+    { key: "27", value: "THB" },
+    { key: "28", value: "TRY" },
+    { key: "29", value: "USD" },
+    { key: "30", value: "ZAR" },
+  ];
 
   const handleSubmit = () => {
     if (selectedCurrency) {
-    return navigation.navigate('Listings')
+      return navigation.navigate("Listings");
     }
-  }
-
+  };
   return (
-    
-    <View >
-          <Button
-            onPress={() => {
-              return navigation.navigate('Login')
-              
-            }}
-            title={"Login"}
-            />
-    <Text style={styles.header}> nuHand </Text>
-    <Text> GBP to </Text>
-    <View style={styles.inputContainer}>
-    <SelectList 
-        setSelected={(val) => setSelectedCurrency(val)} 
-        data={data} 
-        save="value"
-    />
-        </View>
-    <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-          <Text style={styles.buttonText}>Find Listings</Text>
+    <View>
+      <Button
+        onPress={() => {
+          return navigation.navigate("Login");
+        }}
+        title={"Login"}
+      />
+      <Text style={styles.header}> nuHand</Text>
+
+      {conversionValue && (
+        <Text>
+          1 Pound Coin = {conversionValue} {selectedCurrency}
+        </Text>
+      )}
+      <View style={styles.inputContainer}>
+        <SelectList
+          setSelected={(val) => setSelectedCurrency(val)}
+          data={data}
+          save="value"
+        />
+      </View>
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Find Listings</Text>
       </TouchableOpacity>
     </View>
-  )
-  }
+  );
+};
 
-  export default HomeScreen;
+export default HomeScreen;
 
- const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   view: {
     margin: 10,
   },
@@ -86,4 +138,8 @@ const HomeScreen = ( { navigation, selectedCurrency, setSelectedCurrency } ) => 
     paddingVertical: 10,
     borderRadius: 10,
     marginTop: 5,
-  },});
+  },
+});
+function axiosGet(arg0: string) {
+  throw new Error("Function not implemented.");
+}
