436d11aea68af0c9a5f80d16d646207eba913db9
Bakri Khalifa <bakrikhalifa@Bakris-MacBook-Air.local>
2023-01-30 11:45:57 +0000

    added exchange rate API, linked users from firestore to project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-import { NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LogInScreen from "./Screens/LogInScreen";
 import HomeScreen from "./Screens/HomeScreen";
@@ -14,55 +14,81 @@ import Messages from "./Screens/Messages";
 import PersonalInfo from "./Screens/PersonalInfo";
 import MyListings from "./Screens/MyListings";
 import SavedListings from "./Screens/SavedListings";
+import SignUpScreen from "./Screens/SignUpScreen";
+import WelcomeScreen from "./Screens/WelcomeScreen";
 
 type RootStackParamList = {
-  Login: undefined,
-  Home: undefined
+  Login: undefined;
+  Home: undefined;
 };
-
-
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
 
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState([]);
   const [selectedCurrency, setSelectedCurrency] = useState("");
-  
-  
 
-const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
   return (
-   
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name='Login' options={{ title: 'Login' }}>
-  {(props) => <LogInScreen {...props} user={user} setUser={setUser} />}
-  </Stack.Screen>
-        <Stack.Screen name='Home' options={{ title: 'Home' }}>
-  {(props) => <HomeScreen {...props} selectedCurrency={selectedCurrency} setSelectedCurrency={setSelectedCurrency} firstName={firstName} user={user} setUser={setUser} />}
-  </Stack.Screen>
-        <Stack.Screen name="Listings" options={{ title: 'Listings' }}>
-  {(props) => <Listings {...props} selectedCurrency={selectedCurrency} setSelectedCurrency={setSelectedCurrency} />}
-   </Stack.Screen>
-<Stack.Screen name='AddListing' options={{ title: 'AddListing' }}>
-  {(props) => <AddListing {...props} user={user} addListing={AddListing}/>}
-</Stack.Screen>
-        <Stack.Screen name="Account" options={{ title: 'Account' }}>
-        {(props) => <Account {...props} user={user} setUser={setUser} />}
+        <Stack.Screen name="Login" options={{ title: "Login" }}>
+          {(props) => <LogInScreen {...props} user={user} setUser={setUser} />}
         </Stack.Screen>
-        <Stack.Screen name="SavedListings" options={{ title: 'SavedListings' }}>
-        {(props) => <SavedListings {...props} user={user} />}
-        </Stack.Screen>  
-        <Stack.Screen name="PersonalInfo" options={{ title: 'PersonalInfo' }}>
-        {(props) => <PersonalInfo {...props} user={user} />}
-        </Stack.Screen>  
-        <Stack.Screen name="MyListings" options={{ title: 'MyListings' }}>
-        {(props) => <MyListings {...props} user={user} />}
-        </Stack.Screen>  
-        <Stack.Screen name='Messages' options={{ title: 'Messages' }}>
-  {(props) => <Messages {...props}  />}
-  </Stack.Screen>    
+        <Stack.Screen name="Home" options={{ title: "Home" }}>
+          {(props) => (
+            <HomeScreen
+              {...props}
+              selectedCurrency={selectedCurrency}
+              setSelectedCurrency={setSelectedCurrency}
+              firstName={firstName}
+              user={user}
+              setUser={setUser}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Listings" options={{ title: "Listings" }}>
+          {(props) => (
+            <Listings
+              {...props}
+              selectedCurrency={selectedCurrency}
+              setSelectedCurrency={setSelectedCurrency}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="AddListing" options={{ title: "AddListing" }}>
+          {(props) => (
+            <AddListing {...props} user={user} addListing={AddListing} />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Account" options={{ title: "Account" }}>
+          {(props) => <Account {...props} user={user} setUser={setUser} />}
+        </Stack.Screen>
+        <Stack.Screen name="SavedListings" options={{ title: "SavedListings" }}>
+          {(props) => <SavedListings {...props} user={user} />}
+        </Stack.Screen>
+        <Stack.Screen name="PersonalInfo" options={{ title: "PersonalInfo" }}>
+          {(props) => <PersonalInfo {...props} user={user} />}
+        </Stack.Screen>
+        <Stack.Screen name="MyListings" options={{ title: "MyListings" }}>
+          {(props) => <MyListings {...props} user={user} />}
+        </Stack.Screen>
+        <Stack.Screen name="Messages" options={{ title: "Messages" }}>
+          {(props) => <Messages {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="SignUp" options={{ title: "SignUp" }}>
+          {(props) => (
+            <SignUpScreen
+              {...props}
+              firstName={firstName}
+              setFirstName={setFirstName}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Welcome" options={{ title: "Welcome" }}>
+          {(props) => <WelcomeScreen {...props} firstName={firstName} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import { Avatar, ListItem } from "@rneui/themed";
@@ -13,10 +14,71 @@ const HomeScreen = ({
   user,
   setUser,
 }) => {
+
+  interface conversionProps {
+    amount: number;
+    base: string;
+    date: string;
+    rates: { [key: string]: number };
+  }
+
+  const [conversion, setConversion] = useState<conversionProps | null>(null);
+
+  const [conversionValue, setConversionValue] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (conversion !== null) {
+      const value = conversion.rates[selectedCurrency];
+      setConversionValue(value);
+    }
+  }, [selectedCurrency]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.frankfurter.app/latest?from=GBP")
+      .then((res) => {
+        console.log(res.data, "<<<< this one");
+        
+        setConversion(res.data)
+      }).catch(err => {
+        console.log(err, "<<< error");
+        
+      })
+  }, []);
+
+
   // api data:
   const data = [
-    { key: "1", value: "USD" },
-    { key: "2", value: "EUR" },
+    { key: "1", value: "AUD" },
+    { key: "2", value: "BGN" },
+    { key: "3", value: "BRL" },
+    { key: "4", value: "CAD" },
+    { key: "5", value: "CHF" },
+    { key: "6", value: "CNY" },
+    { key: "7", value: "CZK" },
+    { key: "8", value: "DKK" },
+    { key: "9", value: "EUR" },
+    { key: "10", value: "HKD" },
+    { key: "11", value: "HUF" },
+    { key: "12", value: "IDR" },
+    { key: "13", value: "ILS" },
+    { key: "14", value: "INR" },
+    { key: "15", value: "ISK" },
+    { key: "16", value: "JPY" },
+    { key: "17", value: "KRW" },
+    { key: "18", value: "MXN" },
+    { key: "19", value: "MYR" },
+    { key: "20", value: "NOK" },
+    { key: "21", value: "NZD" },
+    { key: "22", value: "PHP" },
+    { key: "23", value: "PLN" },
+    { key: "24", value: "RON" },
+    { key: "25", value: "SEK" },
+    { key: "26", value: "SGD" },
+    { key: "27", value: "THB" },
+    { key: "28", value: "TRY" },
+    { key: "29", value: "USD" },
+    { key: "30", value: "ZAR" },
   ];
 
   const handleSubmit = () => {
@@ -45,7 +107,11 @@ const HomeScreen = ({
       </View>
       <Header />
       <View style={styles.content}>
-        <Text> GBP to </Text>
+      {conversionValue && (
+        <Text>
+          1 Pound Coin = {conversionValue} {selectedCurrency}
+        </Text>
+      )}
         <SelectList
           setSelected={(val) => setSelectedCurrency(val)}
           data={data}
@@ -101,3 +167,6 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
 });
+function axiosGet(arg0: string) {
+  throw new Error("Function not implemented.");
+}
